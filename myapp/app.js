cbe7a92e8201b850e4983c47c4b5f566a3bd2053
vagrant <vagrant@homestead>
2020-05-15 03:01:53 +0000

    add test333.html

--- myapp/app.js
+++ myapp/app.js
@@ -19,8 +19,37 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 写真のサンプルデータ
+var photoList = [
+  {
+      id: "001",
+      name: "photo001.jpg",
+      type: "jpg",
+      dataUrl: "http://localhost:3000/data/photo001.jpg"
+  },{
+      id: "002",
+      name: "photo002.jpg",
+      type: "jpg",
+      dataUrl: "http://localhost:3000/data/photo002.jpg"
+  }
+];
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// 写真リストを取得するAPI
+app.get("/api/photo/list", function(req, res, next){
+  res.json(photoList);
+});
+// app.get("/api/photo/:photoId", function(req, res, next){
+//   var photo;
+//   for (i = 0; i < photoList.length; i++){
+//       if (photoList[i].id == req.params.photoId){
+//           var photo = photoList[i];
+//       }
+//   }
+//   res.json(photo);
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
